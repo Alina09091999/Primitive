e3c19a413c523cc1cf10f41c92d6ba9567b8c271
Alina <alina09091999@yandex.ru>
2023-10-10 19:54:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 7593;
+        int costOfOneMile = 20;
+        int numberOfMiles = amount / costOfOneMile;
+        System.out.println(numberOfMiles);
+    }
+}
